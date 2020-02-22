5d8a46322e76dc7a79933c655959052900e38d02
Aden Herold <aden.herold1@gmail.com>
2020-02-22 14:43:23 +1100

    Feat(feed): Added momentjs dependency. Set post item subheader to moment fromNow time

--- src/pages/Feed/components/PostItem/PostItem.tsx
+++ src/pages/Feed/components/PostItem/PostItem.tsx
@@ -1,18 +1,19 @@
 import React, { FC } from "react";
+import moment from "moment";
 import { Post } from "../../../../graphql/_generated/types";
 import { CardHeader, Avatar, CardContent, Typography } from "@material-ui/core";
 import { Container } from "./Styles";
 
 export interface PostItemProps extends Post {}
 
-const PostItem: FC<PostItemProps> = ({ id, content, createdAt }) => {
+const PostItem: FC<PostItemProps> = ({ content, createdAt }) => {
   return (
     <>
       <Container>
         <CardHeader
           avatar={<Avatar>A</Avatar>}
           title="Aden Herold"
-          subheader={createdAt}
+          subheader={moment(createdAt).fromNow()}
         />
         <CardContent>
           <Typography>{content}</Typography>
